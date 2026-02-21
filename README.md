4eb3dfbdc07bcac14215d9e5a63312db0ba32360
Ruben Capitao <rubencapitao@gmail.com>
2026-02-21 16:43:59 +0000

    Change project structure and update README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ## Description 
 Very simple python program that does this anv that
 
-## Requirements
-Defined in the `requirements.txt` file.
+## Project structure
+The application code can be found under `src` foldeer.
+The unit-tests can be found under `tests` folder.
+Project dependencies are defined in the `requirements.txt` file.
 
+## How to use
+Run `python src/application.py` in your favourite terminal.
+
+## How to test
+Run `pytest` in your favourite terminal.
---
+++ tests/application_test.py
@@ -0,0 +1,27 @@
+from src.application import sendHTTPRequest
+import responses
+
+@responses.activate
+def test_with_mock():
+    responses.add(
+        responses.GET, 
+        "https://api.example.com/data",
+        json={'error': 'not found'}, 
+        status=404
+    )
+    print("test")
+    status = sendHTTPRequest("https://api.example.com/data")
+    assert type(status) == int, "Expecting status to be an integer"
+    assert status == 404, "Expecting status to be 404"
+
+    
+def test_send_correct_url():
+    # Testing the correct link
+    status = sendHTTPRequest("https://simpledebit.gocardless.io/health_check")
+    assert type(status) == int, "Expecting status to be an int"
+    assert status == 200, "Expecting a status code 200"
+
+def test_send_incorrect_url():
+    status = sendHTTPRequest("https://simpledebit.gocardless.io/health_checkz")
+    assert status == 404
+    
