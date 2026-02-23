40131e22e1ee3d2a624f3ac025468f129ffe4272
Ruben Capitao <rubencapitao@gmail.com>
2026-02-23 17:36:24 +0000

    Updated API to use tuple output

--- src/application.py
+++ src/application.py
@@ -2,7 +2,11 @@ import requests
 
 def sendHTTPRequest(url: str):
     response = requests.get(url)
-    return(response.status_code)
+    
+    return(
+        response.status_code, 
+        response.json() if response.headers['content-type'].__contains__("json") else response.content
+    )
 
 if __name__ == '__main__':
     print(sendHTTPRequest('https://simpledebit.gocardless.io/health_check'))
--- tests/application_test.py
+++ tests/application_test.py
@@ -10,18 +10,19 @@ def test_with_mock():
         status=404
     )
     print("test")
-    status = sendHTTPRequest("https://api.example.com/data")
+    status, content = sendHTTPRequest("https://api.example.com/data")
     assert type(status) == int, "Expecting status to be an integer"
     assert status == 404, "Expecting status to be 404"
+    assert 'error' in content and content['error'] == 'not found' 
 
     
 def test_send_correct_url():
     # Testing the correct link
-    status = sendHTTPRequest("https://simpledebit.gocardless.io/health_check")
+    status, content = sendHTTPRequest("https://simpledebit.gocardless.io/health_check")
     assert type(status) == int, "Expecting status to be an int"
     assert status == 200, "Expecting a status code 200"
 
 def test_send_incorrect_url():
-    status = sendHTTPRequest("https://simpledebit.gocardless.io/health_checkz")
+    status, content = sendHTTPRequest("https://simpledebit.gocardless.io/health_checkz")
     assert status == 404
     
